3a4defc39d06c7643f96299c126468e52d817a58
taes2025-tv <tiagosvital@gmail.com>
2025-11-13 18:09:04 +0000

    StartPracticeGameTest

---
+++ Include/scripts/groovy/operations/StartPracticeGame.groovy
@@ -0,0 +1,101 @@
+package operations
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testdata.TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+import internal.GlobalVariable
+
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.By
+
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.webui.driver.DriverFactory
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.util.KeywordUtil
+
+import com.kms.katalon.core.webui.exception.WebElementNotFoundException
+
+import cucumber.api.java.en.And
+import cucumber.api.java.en.Given
+import cucumber.api.java.en.Then
+import cucumber.api.java.en.When
+
+
+
+class StartPracticeGame {
+	/**
+	 * The step definitions below match with Katalon sample Gherkin steps
+	 */
+	@And("I click on the (.*) button to enter as a annonymous user")
+	def I_click_on_the_button_to_enter_as_a_annonymous_user(String string) {
+		WebUI.click(findTestObject('Object Repository/Page_Vite App/' + string))
+	}
+	
+	@When("I click on the (.*) button to start a game")
+	def I_click_on_the_button_to_start_a_game(String string) {
+		WebUI.click(findTestObject('Object Repository/Page_Vite App/' + string))
+	}
+	
+	@Then("I see the bot nine cards")
+	def I_see_the_bot_nine_cards() {
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_1'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_2'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_3'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_4'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_5'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_6'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_7'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_8'))
+		
+	}
+	
+	@And("I see my nine cards")
+	def I_see_my_nine_cards() {
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_1'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_2'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_3'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_4'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_5'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_6'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_7'))
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Trump_card-img_8'))
+		
+	}
+	
+	@And("I see the face down deck")
+	def I_see_the_face_down_deck(){
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_9'))
+		WebUI.verifyElementText(findTestObject('Object Repository/Page_Vite App/span_Practice Game_deck-label text-white mt-1'),'21 cards left')
+	}
+	
+	@And("I see the face up trump")
+	def I_see_the_face_up_trump(){
+		WebUI.verifyElementVisible(findTestObject('Object Repository/Page_Vite App/img_Practice Game_card-img_10'))
+		WebUI.verifyElementText(findTestObject('Object Repository/Page_Vite App/span_Practice Game_deck-label text-white mt-1_1'), 'Trump')
+	}
+
+}
--- Object Repository/Page_Vite App/button_Entrar_inline-flex items-center just_5a503c.rs
+++ Object Repository/Page_Vite App/button_Entrar_inline-flex items-center just_5a503c.rs
@@ -6,12 +6,12 @@
    <elementGuidId>15475b36-bef5-459e-b609-5ffc0af2f453</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>CSS</key>
-         <value></value>
-      </entry>
-      <entry>
          <key>XPATH</key>
          <value>//div[@id='app']/div/div/div[3]/button</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value></value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -23,7 +23,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>button</value>
-      <webElementGuid>a4f76fe3-f13b-43b5-9e44-7be3591d39de</webElementGuid>
+      <webElementGuid>db882ab2-68f7-4b2e-93ed-0d16caf8c769</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -31,7 +31,7 @@
       <name>data-slot</name>
       <type>Main</type>
       <value>button</value>
-      <webElementGuid>0f584976-9d0b-4857-9b90-fc79f88ad452</webElementGuid>
+      <webElementGuid>9cde8c0e-85a2-4412-959a-fdeea162bfaf</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -39,7 +39,7 @@
       <name>class</name>
       <type>Main</type>
       <value>inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg:not([class*='size-'])]:size-4 shrink-0 [&amp;_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 h-9 px-4 py-2 has-[>svg]:px-3 w-full</value>
-      <webElementGuid>fb31db83-a538-40c4-a9c0-75fa1bbbba8b</webElementGuid>
+      <webElementGuid>9949e71a-1076-42df-b35a-376593acac48</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -47,7 +47,7 @@
       <name>text</name>
       <type>Main</type>
       <value> Saltar Login (Modo Anónimo) </value>
-      <webElementGuid>66ed80b0-0152-4f38-be2d-9f9ffc538dfa</webElementGuid>
+      <webElementGuid>ba4dfc76-35ed-4802-b680-c448e88d1043</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -55,7 +55,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;app&quot;)/div[@class=&quot;min-h-screen bg-slate-100 flex items-center justify-center p-4&quot;]/div[@class=&quot;bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm w-full max-w-md&quot;]/div[@class=&quot;items-center px-6 [.border-t]:pt-6 flex flex-col&quot;]/button[@class=&quot;inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg:not([class*='size-'])]:size-4 shrink-0 [&amp;_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80 h-9 px-4 py-2 has-[>svg]:px-3 w-full&quot;]</value>
-      <webElementGuid>f8603778-2907-4c01-827b-8c98c0d6c587</webElementGuid>
+      <webElementGuid>57ae9392-471e-4f5d-860d-5032a9c212f2</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -63,7 +63,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='app']/div/div/div[3]/button</value>
-      <webElementGuid>e5c0f569-7854-461d-8435-bff692e32629</webElementGuid>
+      <webElementGuid>4d0cc3a2-20b5-4750-9cc2-43b8f18fae73</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -71,7 +71,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::button[2]</value>
-      <webElementGuid>3ddc0465-9758-4693-8a04-e11dd6b6b232</webElementGuid>
+      <webElementGuid>94f1e6e5-d7c1-4256-bb66-8482815db766</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -79,7 +79,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>//*/text()[normalize-space(.)='Saltar Login (Modo Anónimo)']/parent::*</value>
-      <webElementGuid>e0850e05-8169-4720-b42d-3ad166b38977</webElementGuid>
+      <webElementGuid>727435c8-afe7-4161-b890-069658aac51c</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -87,7 +87,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[3]/button</value>
-      <webElementGuid>c75ce3a8-3aa2-4d47-9b9d-337c11696a71</webElementGuid>
+      <webElementGuid>93632a61-ef18-4190-ae96-62f7df0bdcbf</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -95,6 +95,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//button[(text() = ' Saltar Login (Modo Anónimo) ' or . = ' Saltar Login (Modo Anónimo) ')]</value>
-      <webElementGuid>dc007dae-d794-455b-aefa-c2cf4dea6e7b</webElementGuid>
+      <webElementGuid>b3ba3b2a-98cf-4c88-b4de-8c1138623eac</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
